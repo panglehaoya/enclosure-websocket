283b815da78d4b4d526898a421d2ff34b6023858
panglehaoya <waxxhgz@163.com>
2022-02-23 14:41:06 +0800

    chore: change-2022年2月23日14:41:04

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -3,37 +3,14 @@ import pkg from "../package.json";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
-// 以下内容会添加到打包结果中
-const footer = `
-if(typeof window !== 'undefined') {
-  window._Dry_VERSION_ = '${pkg.version}'
-}`;
-
 export default {
   input: "./src/index.ts",
   external: ["vue", "vuex"],
   output: [
     {
-      file: pkg.main,
-      format: "cjs",
-      footer,
-    },
-    {
       file: pkg.module,
       format: "esm",
-      footer,
-    },
-    {
-      file: pkg.browser,
-      format: "umd",
-      name: "Dry",
-      footer,
     },
   ],
-  plugins: [
-    typescript(),
-    // 为了打包 dayjs
-    commonjs(),
-    resolve(),
-  ],
+  plugins: [typescript(), commonjs(), resolve()],
 };
