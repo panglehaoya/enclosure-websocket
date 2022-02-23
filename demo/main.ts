1ec7b55fd33040821e74b062d8b25de4e3292920
panglehaoya <waxxhgz@163.com>
2022-02-23 13:43:28 +0800

    chore: change-2022年2月23日13:43:26

--- demo/main.ts
+++ demo/main.ts
@@ -1,5 +1,5 @@
 import Vue from "vue";
 import Index from "./index.vue";
-import { store } from "../store/index";
+import { store } from "../src/store";
 
 new Vue({ store, render: (h) => h(Index) }).$mount("#app");
