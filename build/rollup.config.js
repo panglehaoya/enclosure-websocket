bf865aac9e8267f6b67bf59ba2e1fce1d4350b0b
panglehaoya <waxxhgz@163.com>
2022-02-24 16:27:02 +0800

    chore: delete map files

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -2,8 +2,8 @@ import typescript from "@rollup/plugin-typescript";
 import pkg from "../package.json";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import { babel, getBabelOutputPlugin } from "@rollup/plugin-babel";
-import { uglify } from "rollup-plugin-uglify";
+// import { babel, getBabelOutputPlugin } from "@rollup/plugin-babel";
+// import { uglify } from "rollup-plugin-uglify";
 
 export default {
   input: "src/index.ts",
@@ -11,12 +11,12 @@ export default {
     {
       file: pkg.module,
       format: "esm",
-      plugins: [
-        getBabelOutputPlugin({
-          presets: [["@babel/preset-env", { modules: false }]],
-        }),
-      ],
+      // plugins: [
+      //   getBabelOutputPlugin({
+      //     presets: [["@babel/preset-env", { modules: false }]],
+      //   }),
+      // ],
     },
   ],
-  plugins: [typescript(), commonjs(), resolve(), uglify()],
+  plugins: [typescript(), commonjs(), resolve()],
 };
